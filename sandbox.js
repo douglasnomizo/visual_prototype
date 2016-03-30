5325c1eb2ff2cc634bbdbdd66ad63c9170da59e2
Douglas Nomizo <douglasnomizo@outlook.com>
2016-03-30 14:25:54 -0300

    Add presentation script

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,9 @@
-var container = document.getElementById('container');
-var options = {
-
-}
 var isP = function(d) {
   return d.constructor.prototype === d && d.hasOwnProperty('constructor');
 };
 
 var isF = function(d) {
-  return typeof d === 'function';
+  return typeof d === 'function' && d !== Function.prototype;
 };
 
 var isO = function(d) {
@@ -16,127 +12,11 @@ var isO = function(d) {
 
 
 var getName = function(d) {
-  return d.name || d.constructor.name;
+  if (isO(d)) {
+    return d.name || d.constructor.name + ' sem nome';
+  }
+  return  d.name || d.constructor.name || 'Object';
 }
-
-var Sandbox = (function(el, options) {
-  var config = { width: 1400, height: 1200, charge: -1200, linkDistance: 300, circleSize: 40 }; _.merge(config, options);
-  var nodes = [], links = [];
-
-  var force = d3.layout.force().charge(config.charge).nodes(nodes).links(links).linkDistance(config.linkDistance).size([config.width, config.height]).on('tick', _tick);
-  var svg = d3.select(el).append('svg').attr('width', config.width).attr('height', config.height);
-  var node = svg.selectAll('.node');
-  var link = svg.selectAll('.link');
-
-  function _addId(object) {
-    if (!Object.hasOwnProperty('id')) {
-      object.id = Math.random().toString(16).slice(2);
-    }
-  };
-
-  function _tick() {
-    node.attr('cx', function(d) { return d.x; }).attr('cy', function(d) { return d.y; });
-    link.attr('x1', function(d) { return d.source.x; }).attr('y1', function(d) { return d.source.y; })
-        .attr('x2', function(d) { return d.target.x; }).attr('y2', function(d) { return d.target.y; });
-  }
-
-  function _findPrototype(obj, list) {if (list === undefined) { list = []; } var proto = Object.getPrototypeOf(obj); if (proto === null) {return list; } else {list.push({ source: proto, target: obj }); } return _findPrototype(proto, list); }
-
-  function _updatePrototypeChainFor(newObject) {
-    var prototypes = _findPrototype(newObject);
-
-    nodes = _.union(nodes, prototypes.map(function(p) { return p.source; }));
-    nodes.map(function(obj) { _addId(obj); });
-
-    prototypes.forEach(function(p) {
-      links.push({
-        source: p.source,
-        target: p.target
-      });
-    });
-  }
-
-  function _updateGraph() {
-    link = link.data(links, function(d) { return d.source.id + '-' + d.target.id; });
-    link.enter().insert('line', '.node').attr('class', 'link');
-    link.exit().remove();
-
-    node = node.data(nodes, function(d) { return d.id;});
-    node.enter().append('g').attr('r', config.circleSize)
-      .append('circle')
-      .attr('class', 'node')
-      .attr('r', config.circleSize)
-      .classed('prototype', isP)
-      .classed('object', isO)
-      .classed('function', isF);
-    node.exit().remove();
-
-
-    // node = node.data(nodes, function(d) { return d.id; });
-    // var groups = node.enter().append('g');
-    // node.exit().remove();
-
-
-
-    // node = node.data(nodes, function(d) { return d.id;});
-    // var groups = node.enter().append('g');
-
-    // // var groups = this.svg.selectAll('.node')
-    // //   .data(this.objects)
-    // //   .enter()
-    // //   .insert('g');
-
-
-    // var node = groups.append('circle');
-      // .attr('class', 'node')
-      // .attr('r', config.circleSize)
-      // .classed('prototype', isP)
-      // .classed('object', isO)
-      // .classed('function', isF);
-
-    // groups.append('text')
-    //     .attr('dx', -25)
-    //     .attr('dy', 15)
-    //     .text(getName)
-    //     .style('stroke', 'gray');
-
-    // groups.append('text')
-    //     .attr('dx', -25)
-    //     .attr('dy', -10)
-    //     .text(function(d) {
-    //       if (isP(d)) { return '[Prototype]'; }
-    //       if (isF(d)) { return '[Function]'; }
-    //     })
-    //     .style('stroke', 'gray');
-
-    // node.exit().remove();
-
-
-
-
-    force.start();
-  }
-
-  return {
-    add: function(object) {
-      nodes.push(object);
-      _updatePrototypeChainFor(object);
-    },
-
-    update: function() {
-      _updateGraph();
-    },
-
-    nodes: function() {
-      return nodes;
-    },
-
-    links: function() {
-      return links;
-    }
-  };
-
-}(container, options));
 
 var width = 1400,
     height = 1200;
@@ -145,22 +25,26 @@ function S(el) {
   this.objects = [];
   this.prototype_chain = [];
   this.svg = d3.select(el).append('svg').attr('width', width).attr('height', height);
-  this.force = d3.layout.force().charge(-1200).linkDistance(300).size([width, height]);
+  this.force = d3.layout.force().charge(-1200).linkDistance(380).size([width, height]);
 }
 
 S.prototype.add = function(obj) {
+  this.prototype_chain = [];
   this.objects.push(obj);
   var prototypes = _findPrototype(obj);
 
   this.objects.forEach(function(o) {
     if (o.hasOwnProperty('constructor')) {
       this.objects.push(o.constructor);
-      this.prototype_chain.push({source: o.constructor, target: o, value: 1 });
-      this.prototype_chain.push({source: o, target: o.constructor, value: 2 });
+      this.prototype_chain.push({source: o.constructor, target: o });
+    }
+
+    if (isO(o)) {
+      this.prototype_chain.push({source: o, target: o.__proto__ });
     }
   }, this)
 
-  this.objects = _.union(this.objects, prototypes.map(function(p) { return p.source; }), prototypes.map(function(p) { return p.target; }))
+  this.objects = _.union(this.objects, prototypes.map(function(p) { return p.source; }));
 
   prototypes.forEach(function(p) {
     this.prototype_chain.push({
@@ -169,11 +53,28 @@ S.prototype.add = function(obj) {
     });
   }, this);
 
+  this.update();
 };
 
-function _findPrototype(obj, list) {if (list === undefined) { list = []; } var proto = Object.getPrototypeOf(obj); if (proto === null) {return list; } else {list.push({ source: proto, target: obj }); } return _findPrototype(proto, list); }
+function _findPrototype(obj, list) {
+  if (list === undefined) {
+    list = [];
+  }
+  var proto = Object.getPrototypeOf(obj);
 
-S.prototype.draw = function() {
+  if (proto === null) {
+    return list;
+  } else {
+    list.push({ source: proto, target: obj });
+  }
+
+  return _findPrototype(proto, list);
+}
+
+S.prototype.update = function() {
+  this.svg.selectAll('.link').remove();
+  this.svg.selectAll('.node').remove();
+
   this.force.nodes(this.objects).links(this.prototype_chain).start();
 
   var link = this.svg.selectAll('.link')
@@ -186,38 +87,58 @@ S.prototype.draw = function() {
     .enter()
     .insert('g');
 
-  var node = groups.append('circle')
+  var node = groups.append('rect')
       .attr('class', 'node')
-      .attr('r', 50)
+      .attr('width', 180)
+      .attr('height', 70)
       .classed('prototype', isP)
       .classed('object', isO)
       .classed('function', isF)
       .call(this.force.drag);
 
   groups.append('text')
-      .attr('dx', -25)
+      .attr('dx', 10)
       .attr('dy', 15)
       .text(getName)
       .style('stroke', 'gray');
 
   groups.append('text')
-      .attr('dx', -25)
-      .attr('dy', -10)
+      .attr('dx', 70)
+      .attr('dy', 15)
       .text(function(d) {
         if (isP(d)) { return '[Prototype]'; }
         if (isF(d)) { return '[Function]'; }
       })
       .style('stroke', 'gray');
-    this.force.start();
+
+  groups.append('text')
+      .attr('dx', 5)
+      .attr('dy', 50)
+      .text(function(d) {
+        var text = "";
+        for (x in d) {
+          if (!_.includes(['index', 'x', 'y', 'weight', 'px', 'py', 'fixed'], x)) {
+            if (d.hasOwnProperty(x)) {
+              text += x.toUpperCase() + ' | ';
+            } else {
+              text += x + ' | ';
+            }
+          }
+        }
+        return  text.substring(0, text.length - 2);
+      })
+      .style('stroke', 'gray');
+
+  this.force.start();
 
   this.force.on('tick', function() {
-    link.attr('x1', function(d) { return d.source.x; })
-        .attr('y1', function(d) { return d.source.y; })
-        .attr('x2', function(d) { return d.target.x; })
-        .attr('y2', function(d) { return d.target.y; });
+    link.attr('x1', function(d) { return d.source.x + 75 })
+        .attr('y1', function(d) { return d.source.y + 30 })
+        .attr('x2', function(d) { return d.target.x + 75 })
+        .attr('y2', function(d) { return d.target.y + 30 });
 
-    node.attr('cx', function(d) { return d.x; })
-        .attr('cy', function(d) { return d.y; });
+    node.attr('x', function(d) { return d.x; })
+        .attr('y', function(d) { return d.y; });
 
     d3.selectAll('text')
         .attr('x', function (d) {return d.x; })
@@ -227,34 +148,80 @@ S.prototype.draw = function() {
 
 s = new S(container);
 
-function Cao() {}
-function Gato() {}
 
-cao1 = new Cao();
-cao1.name = 'Dodge';
+//parte 1 - function e new
 
-gato1 = Object.create(Gato.prototype);
-gato1.name = 'Neo';
+function Gato(name){
+  this.name = name
+}
+s.add(Gato.prototype);
 
-gato2 = new Gato();
-gato2.name = 'Lili';
+Gato.prototype.mia = true
+s.update();
 
-gato3 = Object.create(gato1);
-
-s.add(cao1);
+gato1 = new Gato("Felix")
 s.add(gato1);
+
+gato2 = Object.create(Gato.prototype)
 s.add(gato2);
-s.draw();
 
 
-Sandbox.add(cao1);
-Sandbox.update();
+// //sem ligação automática:
+Object.create(null);
+
+// Para criar um objeto, pode-se usar:
+// - new Object
+// - {}
+// - Object.create(Object.prototype);
+
+gato3 = new Object;
+s.add(gato3);
+
+gato3.__proto__ = Gato.prototype
+//  gato3.setPrototypeOf(Gato.prototype)
+s.add(gato3);
 
 
-// nodes.forEach(function(obj) {
-//   if (obj.hasOwnProperty('constructor')) {
-//     nodes.push(obj.constructor);
-//     links.push({source: obj.constructor, target: obj });
-//     links.push({source: obj, target: obj.constructor });
-//   }
-// });
+
+// //parte 2 - as ligações se mantém
+
+Gato.prototype.mia = function(){
+  console.log("Miaaaaaaaaauuuu!!!")
+}
+
+gato1.mia()
+gato2.mia()
+
+
+gato1.__proto__.mia = "sim, algum problema?"
+s.add(gato1)
+
+
+console.log(gato3.mia);
+
+
+// //ou seja: mudando o corpo da função não interferimos via ligações
+
+// //parte3 - conclusão e OLOO
+
+// //não há herança, mas agora temos as ferramentas para "linkar" objetos.
+
+function Animal(){
+}
+
+Animal.prototype.mia = "default"
+Animal.prototype.souAnimal = true
+s.add(Animal.prototype)
+
+gato1.__proto__.__proto__ = Animal.prototype
+s.add(gato1)
+
+// // // prototype chain
+
+gato4 = new Gato("Gato Animal!")
+s.add(gato4)
+
+
+gato5 = Object.create(gato4)
+s.add(gato5)
+
